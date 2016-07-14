f0d783547c904fd1d31f51a1c9e6b73e55be9e5e
Rosangela <ryllachura@gmail.com>
2016-07-14 03:13:12 -0500

    Como primer paso se agrego la funcion de suma de dos digitos

---
+++ EJERCICIO1.py
@@ -0,0 +1,10 @@
+#EJERCICIO PRINCIPAL 1
+import MODULO
+print("*********INVERSION - INTERES*********")
+print("El siguiente programa hara el calculo de la inversion de un monto ingresado.")
+from  MODULO import interes
+interes ()
+from MODULO import repetir
+repetir()
+from MODULO import salir
+salir()
