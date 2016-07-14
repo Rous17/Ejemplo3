45fe5a0449c118cb2a913a27b8630c30f4e1cd0c
Yenifer2016 <yeniferpilar@hotmail.es>
2016-07-14 05:44:08 -0500

    colaboracion parte 2
    mas variables

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -4,9 +4,11 @@ print("*********INVERSION - INTERES*********")
 print("El siguiente programa hara el calculo de la inversion de un monto ingresado.")
 from  MODULO import interes
 interes=input("ingrese el valor:")
+y=input("ingrese un monto: ")
 interes ()
 from MODULO import repetir
-repetir=input("ingrese el monto: ")
+repetir=input("ingrese el monto: ")}
+x=int(input("ingrese un valor"))
 repetir()
 from MODULO import salir
 salir()
